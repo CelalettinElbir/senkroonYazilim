68fcc1a8df51d48ac2b9259e4a37b1d7fea85aa4
Celalettin Elbir <celallelbir@hotmail.com>
2026-01-06 17:18:47 +0300

    fix: Correct model case for Message in MessageResource

--- app/Filament/Resources/MessageResource.php
+++ app/Filament/Resources/MessageResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\MessageResource\Pages;
 use App\Filament\Resources\MessageResource\RelationManagers;
-use App\Models\Message;
+use App\Models\message;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
